5b772809d778a1f671cbb9baae4d63ac7803c0a1
Louise Lee <lj4lee@uwaterloo.ca>
2025-04-13 15:37:56 -0400

    check for missing data

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 path = kagglehub.dataset_download("haiderrasoolqadri/nvidia-corporation-nvda-stock-2015-2024")
 
 print("Path to dataset files:", path)
+
+dataframe = pd.read_csv(path + "/nvidia_stock_2015_to_2024.csv")
+#print(dataframe.head())
+#checking for null data
+print(dataframe.isnull().sum())
