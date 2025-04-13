e5dcebdf58bcbb2f2a39a88acf25849b62f20b27
Louise Lee <lj4lee@uwaterloo.ca>
2025-04-13 15:29:01 -0400

    importing data set

---
+++ main.py
@@ -0,0 +1,8 @@
+import kagglehub
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = kagglehub.dataset_download("haiderrasoolqadri/nvidia-corporation-nvda-stock-2015-2024")
+
+print("Path to dataset files:", path)
