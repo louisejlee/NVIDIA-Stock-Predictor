3b9cd0a43ea6bd5595a0c7fffbfad75de5c0148b
Louise Lee <lj4lee@uwaterloo.ca>
2025-05-06 16:43:57 -0300

    ADF test and differencing

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # NVIDIA-Stock-Predictor
-This project aims to develop a predictive model for forecasting NVDA (NVIDIA Corporation) stock prices using historical stock data.
+This project aims to develop a predictive model for forecasting NVDA (NVIDIA Corporation) stock prices using historical stock data. The dataset, which spans from 2015 to 2024, includes various features such as opening and closing prices, trading volume, and more. The project leverages Python, Pandas, and machine learning models to analyze the data and make predictions about future stock price movements.
+
+Key elements:
+
+- Data cleaning and exploration
+
+- Feature engineering (e.g., moving averages, daily returns)
+
+- Building a predictive model using regression and machine learning techniques
+
+- Visualizations to analyze trends and patterns in the stock data
+
+Tools & Libraries:
+
+- Python
+
+- Pandas
+
+- Matplotlib & Seaborn for visualization
+
+- scikit-learn for machine learning models
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import kagglehub
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 path = kagglehub.dataset_download("haiderrasoolqadri/nvidia-corporation-nvda-stock-2015-2024")
 
@@ -27,7 +28,32 @@ plt.title('NVDA Stock Closing Price')
 plt.xlabel('Date')
 plt.ylabel('Closing Price (USD)')
 plt.legend()
-plt.show()
+#plt.show()
+
+# check for stationary using Augmented Dickey-Fuller test
+from statsmodels.tsa.stattools import adfuller
+result = adfuller(dataframe['close'])
+print("ADF statistic: ", result[0])
+print("p-value: ", result[1])
+# reject null hypothesis if p<0.05, meaning data is stationary
+# adf stat = 5.114
+# p-value = 1.0
+
+#thus we must do differencing to make data stationary
+df_diff = dataframe['close'].diff().dropna()
+
+#rerun ADF test
+result2 = adfuller(df_diff)
+print("ADF statistic: ", result2[0])
+print("p-value: ", result2[1])
+# adf stat = -6.803
+# p-value = 2.207e-09
+
+# data is now stationary!
+
+
+
+
 
 
 
